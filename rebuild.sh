8e7c65a2866507cdc18a133880b986791742b556
Keith Gaughan <k@stereochro.me>
2015-02-16 12:29:51 +0000

    DNSSEC ALL THE THINGS!

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
-for zonefile in $(find $(dirname $0) -depth 1 -type f -and ! \( -name README.md -or -name .gitignore -or -name rebuild.sh \) -exec basename {} \;); do
+
+ODS_ROOT=/usr/local/var/opendnssec
+NSD_ZONES=/usr/local/etc/nsd/zones
+
+get_zones () {
+	# Unsigned zones have the lowest priority, so we link them first.
+	find $ODS_ROOT/unsigned -depth 1 -type f \
+		-and ! \( -name README.md -or -name .gitignore -or -name rebuild.sh \)
+	# Then we link signed zones, which will overwrite the links for any
+	# unsigned zones.
+	find $ODS_ROOT/signed -depth 1 -type f
+}
+
+get_zones | while read zone; do
+	ln -s -f $zone $NSD_ZONES/$(basename $zone).zone
+done
+
+for zonefile in $NSD_ZONES/*; do
 	cat <<EOZ
 zone:
-	name: "$zonefile"
+	name: "$(basename $zonefile .zone)"
 	include-pattern: "master"
 EOZ
 done > /usr/local/etc/nsd/zones.conf
--- talideon.com
+++ talideon.com
@@ -2,7 +2,7 @@ $ORIGIN		talideon.com.
 $TTL		3600
 
 @			SOA		ns1 k.stereochro.me. (
-					2014060701 ; serial
+					2015021601 ; serial
 					6h         ; refresh
 					1h         ; retry
 					1w         ; expire
@@ -27,6 +27,13 @@ ns2			AAAA	2a01:4f8:101:23a4::2
 ; Server itself.
 lir			A		78.153.202.217
 lir			AAAA	2a01:a8:201::217
+
+lir			SSHFP 1 1 f08cd3d31d3be6bc2687a38f0edc7881e99bc207
+lir			SSHFP 1 2 b4d609295c470eafa42f8d3e2f94765a5523a0d9b1bc07e01ab97bcad17e0cb3
+lir			SSHFP 2 1 17716e30fef9653801fc1a42deba46624e5d9491
+lir			SSHFP 2 2 5abfbfa16b91c9fac1683656743743f0aedab0e24cf78cf0e60df7b768d2fd25
+lir			SSHFP 3 1 873c8651b2daf6cfc8273dadbf77b46edc181a42
+lir			SSHFP 3 2 8efc13f18be81ec12a09e36c7b0b153bccea4f00b5102fc23ddd5042f5a225d3
 
 ; Aliases.
 bethisad	CNAME	@
